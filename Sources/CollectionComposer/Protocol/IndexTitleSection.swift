ebea2a19d6aef192c3fd6b95520af859228e1b64
koshimizu-sansan <takehito.koshimizu@sansan.com>
2024-10-03 19:00:55 +0900

    Add Sendable

--- Sources/CollectionComposer/Protocol/IndexTitleSection.swift
+++ Sources/CollectionComposer/Protocol/IndexTitleSection.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// IndexTitledSection allows to show index title at the right side of the collection view.
-public protocol IndexTitleSection: Section {
+public protocol IndexTitleSection: Section, Sendable {
     /// A title for the section. Each title will be displayed as index titles.
     var title: String? { get set }
 
